930b6fcd948f50eb8d2a7f090ff36d76fa93dc03
Matheus <torresmiranda.rj@gmail.com>
2023-06-20 16:34:48 -0300

    feat: check if a tool has already been registered with the same title or link

--- src/app/use-cases/tool/create-tool/index.ts
+++ src/app/use-cases/tool/create-tool/index.ts
@@ -1,18 +1,43 @@
-import { CreateToolRepository } from '@data/repositories/tool';
+import {
+	CreateToolRepository,
+	FindToolByLinkRepository,
+	FindToolByTitleRepository,
+} from '@data/repositories/tool';
 
 import { Tool } from '@domain/entities/tool.entity';
+import { ToolFoundError } from '@domain/errors/tool/tool-found.error';
 import {
 	ICreateToolRequest,
 	ICreateToolResponse,
 	ICreateToolUseCase,
 } from '@domain/use-cases/tool/create-tool.use.case';
 
-type ToolRepository = CreateToolRepository;
+type ToolRepository = CreateToolRepository &
+	FindToolByLinkRepository &
+	FindToolByTitleRepository;
 
 export class CreateToolUseCase implements ICreateToolUseCase {
 	constructor(private toolRepository: ToolRepository) {}
 
 	public async exec(request: ICreateToolRequest): Promise<ICreateToolResponse> {
+		const toolFoundByTitle = await this.toolRepository.findByTitle(
+			request.title,
+		);
+
+		if (toolFoundByTitle) {
+			throw new ToolFoundError(
+				`Tool already exists with title "${request.title}".`,
+			);
+		}
+
+		const toolFoundByLink = await this.toolRepository.findByLink(request.link);
+
+		if (toolFoundByLink) {
+			throw new ToolFoundError(
+				`Tool already exists with link "${request.link}".`,
+			);
+		}
+
 		const tool = new Tool(request);
 
 		await this.toolRepository.create(tool);
