348ac3d3707997cb943bec445ca5bec5a7c22b8d
Matheus <torresmiranda.rj@gmail.com>
2023-06-20 18:55:31 -0300

    test: ensure SUT list all registered tools

--- src/app/use-cases/tool/list-tools/index.spec.ts
+++ src/app/use-cases/tool/list-tools/index.spec.ts
@@ -2,7 +2,10 @@ import { MockProxy, mock } from 'jest-mock-extended';
 
 import { ListToolsRepository } from '@data/repositories/tool';
 
+import { Tool } from '@domain/entities/tool.entity';
 import { ToolNotFoundError } from '@domain/errors/tool/tool-not-found.error';
+
+import { makeTool } from '@tests/factories/entities/tool.factory';
 
 import { ListToolsUseCase } from '.';
 
@@ -10,13 +13,32 @@ describe('ListTools [use case]', (): void => {
 	let toolRepository: MockProxy<ListToolsRepository>;
 	let sut: ListToolsUseCase;
 
-	beforeAll((): void => {
-		toolRepository = mock();
+	const tools: Tool[] = [];
 
-		toolRepository.listTools.mockResolvedValueOnce([]);
+	beforeAll((): void => {
+		tools.push(
+			makeTool({
+				title: 'Notion',
+				link: 'https://notion.so',
+			}),
+			makeTool({
+				title: 'fastify',
+				link: 'https://www.fastify.io/',
+			}),
+			makeTool({
+				title: 'json-server',
+				link: 'https://github.com/typicode/json-server',
+			}),
+		);
+
+		toolRepository = mock();
 	});
 
 	beforeEach((): void => {
+		toolRepository.listTools
+			.mockResolvedValueOnce([])
+			.mockResolvedValueOnce(tools);
+
 		sut = new ListToolsUseCase(toolRepository);
 	});
 
@@ -28,5 +50,16 @@ describe('ListTools [use case]', (): void => {
 		);
 
 		expect(toolRepository.listTools).toHaveBeenCalledTimes(1);
+	});
+
+	it('should list all tools', async (): Promise<void> => {
+		const { tools: data } = await sut.exec();
+
+		expect(toolRepository.listTools).toHaveBeenCalledTimes(2);
+
+		expect(data.length).toBe(3);
+		expect(data[0]).toEqual(tools[0]);
+		expect(data[1]).toEqual(tools[1]);
+		expect(data[2]).toEqual(tools[2]);
 	});
 });
