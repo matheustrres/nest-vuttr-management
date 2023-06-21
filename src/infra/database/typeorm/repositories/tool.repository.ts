89b686dae0941af727f855dc8f204263784ceebc
Matheus <torresmiranda.rj@gmail.com>
2023-06-21 19:40:30 -0300

    feat: create typeorm tool repository implementing domain repository contracts

---
+++ src/infra/database/typeorm/repositories/tool.repository.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@nestjs/common';
+import { Repository, ArrayContains } from 'typeorm';
+
+import { dataSource } from '../datasource';
+import { TypeORMToolEntity } from '../entities/tool.entity';
+
+import {
+	CreateToolRepository,
+	DeleteToolRepository,
+	FindToolByIdRepository,
+	FindToolByLinkRepository,
+	FindToolByTitleRepository,
+	ListToolsRepository,
+} from '@data/repositories/tool';
+
+import { Tool } from '@domain/entities/tool.entity';
+
+type ToolRepository = CreateToolRepository &
+	DeleteToolRepository &
+	FindToolByIdRepository &
+	FindToolByLinkRepository &
+	FindToolByTitleRepository &
+	ListToolsRepository;
+
+@Injectable()
+export class TypeORMToolRepository implements ToolRepository {
+	private repository: Repository<TypeORMToolEntity>;
+
+	constructor() {
+		this.repository = dataSource.getRepository(TypeORMToolEntity);
+	}
+
+	public async create(tool: Tool): Promise<void> {
+		await this.repository.save(tool);
+	}
+
+	public async delete(id: string): Promise<void> {
+		await this.repository.delete(id);
+	}
+
+	public async findById(id: string): Promise<Tool | null> {
+		return this.repository.findOne({
+			where: {
+				id,
+			},
+		});
+	}
+
+	public async findByLink(link: string): Promise<Tool | null> {
+		return this.repository.findOne({
+			where: {
+				link,
+			},
+		});
+	}
+
+	public async findByTitle(title: string): Promise<Tool | null> {
+		return this.repository.findOne({
+			where: {
+				title,
+			},
+		});
+	}
+
+	public async listTools(
+		input?: { tag?: string | null } | undefined,
+	): Promise<Tool[]> {
+		return this.repository.find({
+			...(input?.tag && {
+				where: {
+					tags: ArrayContains([input.tag]),
+				},
+			}),
+		});
+	}
+}
