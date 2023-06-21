ca2f7019dd784d64eb4edda4daaa4c0c26aee6be
Matheus <torresmiranda.rj@gmail.com>
2023-06-20 22:41:53 -0300

    refactor: override standard values with incoming props

--- tests/factories/entities/tool.factory.ts
+++ tests/factories/entities/tool.factory.ts
@@ -8,5 +8,6 @@ export const makeTool = (props: Partial<GetProps<Tool>> = {}): Tool => {
 			'Where over 100 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories...',
 		link: 'https://github.com/',
 		tags: ['github', 'coding', 'developers', 'open-source', 'community'],
+		...props,
 	});
 };
