3d843cd53a60505891b9608de066ff4c45420b84
Matheus <torresmiranda.rj@gmail.com>
2023-06-20 21:00:23 -0300

    chore: create FindToolByTitleRepository contract

--- src/data/repositories/tool/find-tool.repository.ts
+++ src/data/repositories/tool/find-tool.repository.ts
@@ -2,10 +2,14 @@ import { Tool } from '@domain/entities/tool.entity';
 
 type FindByToolOutput = Tool | undefined;
 
-export interface FindToolByTitleRepository {
-	findByTitle: (title: string) => Promise<FindByToolOutput>;
+export interface FindToolByIdRepository {
+	findById: (id: string) => Promise<FindByToolOutput>;
 }
 
 export interface FindToolByLinkRepository {
 	findByLink: (link: string) => Promise<FindByToolOutput>;
+}
+
+export interface FindToolByTitleRepository {
+	findByTitle: (title: string) => Promise<FindByToolOutput>;
 }
