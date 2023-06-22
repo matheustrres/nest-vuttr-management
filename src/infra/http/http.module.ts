cb3c148beaff7f301a52fa1730c99f67b72eeb00
Matheus <torresmiranda.rj@gmail.com>
2023-06-22 11:18:27 -0300

    feat: create HTTPModule

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,55 @@
+import { Module } from '@nestjs/common';
+
+import { ToolController } from './presenters/controllers/tool.controller';
+
+import { CreateToolUseCase } from '@app/use-cases/tool/create-tool';
+import { DeleteToolUseCase } from '@app/use-cases/tool/delete-tool';
+import { ListToolsUseCase } from '@app/use-cases/tool/list-tools';
+
+import {
+	CreateToolRepository as CreateToolRepositoryContract,
+	DeleteToolRepository as DeleteToolRepositoryContract,
+	FindToolByIdRepository as FindToolByIdRepositoryContract,
+	FindToolByLinkRepository as FindToolByLinkRepositoryContract,
+	FindToolByTitleRepository as FindToolByTitleRepositoryContract,
+	ListToolsRepository as ListToolsRepositoryContract,
+} from '@data/repositories/tool';
+
+import { DatabaseModule } from '@infra/database/database.module';
+
+type CreateToolRepository = CreateToolRepositoryContract &
+	FindToolByLinkRepositoryContract &
+	FindToolByTitleRepositoryContract;
+type DeleteToolRepository = DeleteToolRepositoryContract &
+	FindToolByIdRepositoryContract;
+
+@Module({
+	imports: [DatabaseModule],
+	providers: [
+		{
+			provide: CreateToolUseCase,
+			useFactory: (toolRepository: CreateToolRepository): CreateToolUseCase =>
+				new CreateToolUseCase(toolRepository),
+			inject: [
+				CreateToolRepositoryContract,
+				FindToolByLinkRepositoryContract,
+				FindToolByTitleRepositoryContract,
+			],
+		},
+		{
+			provide: DeleteToolUseCase,
+			useFactory: (toolRepository: DeleteToolRepository): DeleteToolUseCase =>
+				new DeleteToolUseCase(toolRepository),
+			inject: [DeleteToolRepositoryContract, FindToolByIdRepositoryContract],
+		},
+		{
+			provide: ListToolsUseCase,
+			useFactory: (
+				toolRepository: ListToolsRepositoryContract,
+			): ListToolsUseCase => new ListToolsUseCase(toolRepository),
+			inject: [ListToolsRepositoryContract],
+		},
+	],
+	controllers: [ToolController],
+})
+export class HTTPmodule {}
