770888eb966fa9e81f5b95b20ee40e6c27258118
Matheus <torresmiranda.rj@gmail.com>
2023-06-25 17:05:10 -0300

    feat: hash password before saving it to the database

--- src/app/use-cases/user/create-user/index.ts
+++ src/app/use-cases/user/create-user/index.ts
@@ -1,3 +1,4 @@
+import { HashString } from '@data/contracts/hash';
 import { CreateUserRepository } from '@data/contracts/repositories/user/create-user.repository';
 import { FindUserByEmailRepository } from '@data/contracts/repositories/user/find-user.repository';
 
@@ -10,9 +11,10 @@ import {
 } from '@domain/use-cases/user/create-user.use-case';
 
 type UserRepository = CreateUserRepository & FindUserByEmailRepository;
+type Hasher = HashString;
 
 export class CreateUserUseCase implements ICreateUserUseCase {
-	constructor(private userRepository: UserRepository) {}
+	constructor(private hasher: Hasher, private userRepository: UserRepository) {}
 
 	public async exec(request: ICreateUserRequest): Promise<ICreateUserResponse> {
 		const userFoundByEmail = await this.userRepository.findByEmail(
@@ -25,7 +27,12 @@ export class CreateUserUseCase implements ICreateUserUseCase {
 			);
 		}
 
-		const user = new User(request);
+		const password = await this.hasher.hashString(request.password);
+
+		const user = new User({
+			...request,
+			password,
+		});
 
 		await this.userRepository.create(user);
 
