d8a831a9987be2ac9bfbcdc951b3812efd338b4e
Matheus <torresmiranda.rj@gmail.com>
2023-06-26 17:34:46 -0300

    chore: provide user-related repositories

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -5,7 +5,9 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { DatabaseService } from './database.service';
 import { dataSource } from './typeorm/datasource';
 import { TypeORMToolRepository } from './typeorm/repositories/tool.repository';
+import { TypeORMUserRepository } from './typeorm/repositories/user.repository';
 
+// tool repositories
 import {
 	CreateToolRepository,
 	DeleteToolRepository,
@@ -14,6 +16,11 @@ import {
 	FindToolByTitleRepository,
 	ListToolsRepository,
 } from '@data/contracts/repositories/tool';
+// user repositories
+import {
+	CreateUserRepository,
+	FindUserByEmailRepository,
+} from '@data/contracts/repositories/user';
 
 @Module({
 	imports: [
@@ -31,6 +38,7 @@ import {
 		}),
 	],
 	providers: [
+		// tool-related
 		{
 			provide: CreateToolRepository,
 			useClass: TypeORMToolRepository,
@@ -55,14 +63,27 @@ import {
 			provide: ListToolsRepository,
 			useClass: TypeORMToolRepository,
 		},
+		// user-related
+		{
+			provide: CreateUserRepository,
+			useClass: TypeORMUserRepository,
+		},
+		{
+			provide: FindUserByEmailRepository,
+			useClass: TypeORMUserRepository,
+		},
 	],
 	exports: [
+		// tool-related
 		CreateToolRepository,
 		DeleteToolRepository,
 		FindToolByIdRepository,
 		FindToolByLinkRepository,
 		FindToolByTitleRepository,
 		ListToolsRepository,
+		// user-related
+		CreateUserRepository,
+		FindUserByEmailRepository,
 	],
 })
 export class DatabaseModule {}
