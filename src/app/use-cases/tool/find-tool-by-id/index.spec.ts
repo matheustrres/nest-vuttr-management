8d3996e19e9cc602f9d9c7c322a8239dafc9068c
Matheus <torresmiranda.rj@gmail.com>
2023-07-03 19:23:19 -0300

    refactor(test): inject cache manager

--- src/app/use-cases/tool/find-tool-by-id/index.spec.ts
+++ src/app/use-cases/tool/find-tool-by-id/index.spec.ts
@@ -1,7 +1,9 @@
 import { MockProxy, mock } from 'jest-mock-extended';
 
+import { GetCacheKey, SetCacheKey } from '@data/contracts/cache';
 import { FindToolByIdRepository } from '@data/contracts/repositories/tool';
 
+import { Tool } from '@domain/entities/tool.entity';
 import { ToolNotFoundError } from '@domain/errors/tool/tool-not-found.error';
 
 import { makeTool } from '@tests/factories/entities/tool.factory';
@@ -9,26 +11,42 @@ import { makeTool } from '@tests/factories/entities/tool.factory';
 import { FindToolByIdUseCase } from '.';
 
 describe('FindToolById [use case]', (): void => {
+	let cacheManager: MockProxy<GetCacheKey & SetCacheKey>;
 	let toolRepository: MockProxy<FindToolByIdRepository>;
 	let sut: FindToolByIdUseCase;
 
+	const tools: Tool[] = [
+		makeTool({
+			id: '2nd_random_tool_id',
+			userId: '2nd_random_user_id',
+			title: 'Notion',
+		}),
+		makeTool({
+			id: '3rd_random_tool_id',
+			userId: '3rd_random_user_id',
+			title: 'Github',
+		}),
+	];
+
 	beforeAll((): void => {
+		cacheManager = mock();
 		toolRepository = mock();
 
-		toolRepository.findById.mockResolvedValueOnce(null).mockResolvedValueOnce(
-			makeTool({
-				id: 'random_tool_id',
-				title: 'random_tool_title',
-				userId: 'random_user_id',
-			}),
-		);
+		cacheManager.get
+			.mockResolvedValueOnce(undefined)
+			.mockResolvedValueOnce(undefined)
+			.mockResolvedValueOnce(tools[1]);
+
+		toolRepository.findById
+			.mockResolvedValueOnce(null)
+			.mockResolvedValueOnce(tools[0]);
 	});
 
 	beforeEach((): void => {
-		sut = new FindToolByIdUseCase(toolRepository);
+		sut = new FindToolByIdUseCase(cacheManager, toolRepository);
 	});
 
-	it('should throw when searching for a tool with an invalid id', async (): Promise<void> => {
+	it('should throw when searching for a non-existing tool', async (): Promise<void> => {
 		const promise = sut.exec({
 			id: 'fake_tool_id',
 			userId: 'fake_user_id',
@@ -38,6 +56,9 @@ describe('FindToolById [use case]', (): void => {
 			new ToolNotFoundError('No tool were found with id "fake_tool_id".'),
 		);
 
+		const key = '--vuttr/users:fake_user_id/tools:fake_tool_id';
+
+		expect(cacheManager.get).toHaveBeenNthCalledWith(1, key);
 		expect(toolRepository.findById).toHaveBeenNthCalledWith(1, {
 			id: 'fake_tool_id',
 			userId: 'fake_user_id',
@@ -46,18 +67,41 @@ describe('FindToolById [use case]', (): void => {
 
 	it('should return a tool', async (): Promise<void> => {
 		const { tool } = await sut.exec({
-			id: 'random_tool_id',
-			userId: 'random_user_id',
+			id: '2nd_random_tool_id',
+			userId: '2nd_random_user_id',
 		});
 
+		const key = '--vuttr/users:2nd_random_user_id/tools:2nd_random_tool_id';
+
+		expect(cacheManager.get).toHaveBeenNthCalledWith(2, key);
 		expect(toolRepository.findById).toHaveBeenNthCalledWith(2, {
-			id: 'random_tool_id',
-			userId: 'random_user_id',
+			id: '2nd_random_tool_id',
+			userId: '2nd_random_user_id',
+		});
+		expect(cacheManager.set).toHaveBeenNthCalledWith(1, key, tools[0]);
+
+		expect(tool.id).toBe('2nd_random_tool_id');
+		expect(tool.title).toBe('Notion');
+		expect(tool.userId).toBe('2nd_random_user_id');
+	});
+
+	it('should return a cached tool', async (): Promise<void> => {
+		const { tool } = await sut.exec({
+			id: '3rd_random_tool_id',
+			userId: '3rd_random_user_id',
 		});
 
-		expect(tool.id).toBeDefined();
-		expect(tool.id).toBe('random_tool_id');
-		expect(tool.title).toBe('random_tool_title');
-		expect(tool.userId).toBe('random_user_id');
+		const key = '--vuttr/users:3rd_random_user_id/tools:3rd_random_tool_id';
+
+		expect(cacheManager.get).toHaveBeenNthCalledWith(3, key);
+		expect(toolRepository.findById).not.toHaveBeenNthCalledWith(3, {
+			id: '3rd_random_tool_id',
+			userId: '3rd_random_user_id',
+		});
+		expect(cacheManager.set).not.toHaveBeenCalledTimes(2);
+
+		expect(tool.id).toBe('3rd_random_tool_id');
+		expect(tool.title).toBe('Github');
+		expect(tool.userId).toBe('3rd_random_user_id');
 	});
 });
