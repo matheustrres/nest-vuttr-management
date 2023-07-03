b7c39c8c3ad1b02a06c106c40fabe90602bc24e0
Matheus <torresmiranda.rj@gmail.com>
2023-07-03 11:38:06 -0300

    refactor: use nest jwt service to sign token

--- src/infra/http/auth/interceptors/jwt-token.interceptor.ts
+++ src/infra/http/auth/interceptors/jwt-token.interceptor.ts
@@ -4,10 +4,9 @@ import {
 	Injectable,
 	NestInterceptor,
 } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
 import { Observable, map } from 'rxjs';
-
-import { signJWTToken } from '../helpers/sign-jwt-token.helper';
 
 import { cookieConfig } from '@config/cookie.config';
 
@@ -15,14 +14,18 @@ import { User } from '@domain/entities/user.entity';
 
 @Injectable()
 export class JWTTokenInterceptor implements NestInterceptor {
+	constructor(private readonly jwtService: JwtService) {}
+
 	public intercept(
 		context: ExecutionContext,
 		next: CallHandler<any>,
 	): Observable<any> {
 		return next.handle().pipe(
 			map((user: User): User => {
+				const token = this.jwtService.sign({
+					sub: user.email,
+				}) as string;
 				const response = context.switchToHttp().getResponse() as Response;
-				const token = signJWTToken(user.email);
 
 				response.setHeader('Authorization', `Bearer ${token}`);
 				response.cookie('token', token, cookieConfig);
